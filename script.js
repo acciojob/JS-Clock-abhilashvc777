fc98bbd8abceeee78dea5f87276bd7f20f90f744
abhilashvc777 <145086618+abhilashvc777@users.noreply.github.com>
2025-02-07 19:17:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 //your code here
+const hour = document.querySelector(".hour-hand")
+const minute = document.querySelector(".min-hand")
+const second = document.querySelector(".second-hand") 
+let date = new Date()
+let sec = date.getSeconds()
+let min = date.getMinutes()
+let hor = date.getHours()
+let b = 90+6*min
+minute.style.transform = `rotate(${b}deg)`
+let d = 90+6*hor+5*hor
+hour.style.transform = `rotate(${d}deg)`
+setInterval(()=>{
+	let date = new Date()
+	let sec = date.getSeconds()
+	let min = date.getMinutes()
+	let hor = date.getHours()
+	console.log(hor)
+	if(sec==0){
+		sec=60
+	}
+	let a = 90+6*sec
+	second.style.transform = `rotate(${a}deg)`
+	if(a==450){
+	   if(min==0){
+			min = 60
+		}
+	let b = 90+6*min
+	minute.style.transform = `rotate(${b}deg)`
+		if(b==450){
+			let c = 90+6*hor+5*hor
+			hour.style.transform = `rotate(${c}deg)`
+		}
+	}
+},1000) 
