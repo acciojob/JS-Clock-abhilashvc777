da2f89c1817d59d1660d028996dee7fb77d0625d
abhilashvc777 <145086618+abhilashvc777@users.noreply.github.com>
2025-02-07 22:05:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,12 +12,24 @@ if(hor>12){
 	hor = hor-12
 }
 let d = 90+hor*5*6
+if(min>=1 && min<=15){
+	d = 90+hor*5*6+6
+}else if(min>=16 && min<=30){
+	d =  90+hor*5*6+12
+}else if(min>=31 && min<=45){
+	d = 90+hor*5*6+18
+}else if(min>=45 && min<=59){
+	d = 90+hor*5*6+24
+} else{
+	d = 90+hor*5*6
+}
+console.log(d)
 hour.style.transform = `rotate(${d}deg)`
 setInterval(()=>{
 	let date = new Date()
 	let sec = date.getSeconds()
 	let min = date.getMinutes()
-	let hor = date.getHours()
+	// let hor = date.getHours()
 	if(sec==0){
 		sec=60
 	}
@@ -29,9 +41,20 @@ setInterval(()=>{
 		}
 	let b = 90+6*min
 	minute.style.transform = `rotate(${b}deg)`
-		if(b==450){
-			let c = 90+hor*5*6
-			hour.style.transform = `rotate(${c}deg)`
-		}
 	}
+	console.log(hor)
+	let c = 90+hor*5*6
+	    if(min>=1 && min<=15){
+	      c = 90+hor*5*6+6
+         }else if(min>=16 && min<=30){
+	      c =  90+hor*5*6+12
+         }else if(min>=31 && min<=45){
+	      c = 90+hor*5*6+18
+        } else if(min>=45 && min<=59){
+	      c = 90+hor*5*6+24
+        } else{
+	      c = 90+hor*5*6
+       }
+			console.log(c)
+			hour.style.transform = `rotate(${c}deg)`
 },1000) 
